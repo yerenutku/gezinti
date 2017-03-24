ec3cdda21b0bcdc9fb0ecc0e990e94fdb2c992a2
Yasin Arslan <yyasinarslan@gmail.com>
2017-03-25 01:52:25 +0300

    [dev][map-feature][ -added mapfragment, mapview and implementation -added permissions into manifest ]

--- android/Gezinti/app/src/main/java/com/hackathon/gezinti/MainActivity.java
+++ android/Gezinti/app/src/main/java/com/hackathon/gezinti/MainActivity.java
@@ -3,11 +3,20 @@ package com.hackathon.gezinti;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.hackathon.gezinti.ui.MapFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fl_map_container, new MapFragment(), "")
+                    .commit();
+        }
+
     }
 }
---
+++ android/Gezinti/app/src/main/java/com/hackathon/gezinti/ui/MapFragment.java
@@ -0,0 +1,82 @@
+package com.hackathon.gezinti.ui;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.hackathon.gezinti.R;
+
+public class MapFragment extends Fragment implements OnMapReadyCallback {
+
+    /*  Required for Google Maps  */
+    MapView mapView;
+    GoogleMap googleMap;
+
+    public MapFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
+        mapView = (MapView) view.findViewById(R.id.mv_maps);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+
+        return view;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.googleMap = googleMap;
+        this.googleMap.getUiSettings().setZoomControlsEnabled(true);
+        this.googleMap.getUiSettings().setZoomGesturesEnabled(true);
+    }
+
+    private void experimental(){
+        MapsInitializer.initialize(this.getActivity());
+        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+        builder.include(new LatLng(55.854049, 13.661331));
+        LatLngBounds bounds = builder.build();
+        int padding = 0;
+        // Updates the location and zoom of the MapView
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngBounds(bounds, padding);
+        googleMap.moveCamera(cameraUpdate);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if(mapView != null) mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if(mapView != null) mapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if(mapView != null) mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        if(mapView != null) mapView.onLowMemory();
+    }
+
+}
