3763a1cdf09f3a0ec6142c169b8a9367cde2fccd
Yasin Arslan <yyasinarslan@gmail.com>
2017-03-25 14:04:27 +0300

    [dev][map-feature][ -edited new refresh button -edited ui package deleted and mapfragment transported to fragment package ]

--- android/Gezinti/app/src/main/java/com/hackathon/gezinti/MainActivity.java
+++ android/Gezinti/app/src/main/java/com/hackathon/gezinti/MainActivity.java
@@ -4,17 +4,17 @@ import android.os.Bundle;
 import android.support.design.widget.BottomSheetBehavior;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
 
 import com.hackathon.gezinti.fragment.BottomSheetListFragment;
-import com.hackathon.gezinti.ui.MapFragment;
+import com.hackathon.gezinti.fragment.MapFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     private BottomSheetBehavior<View> mBottomSheetBehavior;
     private BottomSheetListFragment mBottomSheetListFragment;
 
-    private MapFragment mapFragment;
+    private MapFragment mMapFragment;
+    private Button mButtonRefresh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,36 +23,31 @@ public class MainActivity extends AppCompatActivity {
         initViews();
 
         if(savedInstanceState == null) {
-            mapFragment = new MapFragment();
+            mMapFragment = new MapFragment();
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.fl_map_container, mapFragment, "")
+                    .add(R.id.fl_map_container, mMapFragment, "")
                     .commit();
         }
 
     }
 
     private void initViews() {
-
         mBottomSheetListFragment = (BottomSheetListFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentBottomSheet);
         mBottomSheetListFragment.setDimBackground(findViewById(R.id.dim_background));
         mBottomSheetBehavior  = BottomSheetBehavior.from(findViewById(R.id.fragmentBottomSheet));
         mBottomSheetBehavior.setHideable(true);
         mBottomSheetBehavior.setBottomSheetCallback(mBottomSheetListFragment.getBottomSheetCallback());
+        mButtonRefresh = (Button) findViewById(R.id.btn_refresh);
+        mButtonRefresh.setOnClickListener(this);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getItemId() == R.id.action_search){
-            if(mapFragment.isAdded()){
-                mapFragment.makeRequestAndPin();
+    public void onClick(View v) {
+        if(v.getId() == mButtonRefresh.getId()){
+            if(mMapFragment.isAdded()){
+                mMapFragment.makeRequestAndPin();
             }
         }
-        return super.onOptionsItemSelected(item);
     }
+
 }
