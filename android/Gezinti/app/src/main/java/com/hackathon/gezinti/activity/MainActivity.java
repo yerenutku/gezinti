cadd40edfb30612c1f2c78dda21f2f63ed5cdf28
Yasin Arslan <yyasinarslan@gmail.com>
2017-10-19 00:16:09 +0300

    -map's clean version

--- android/Gezinti/app/src/main/java/com/hackathon/gezinti/activity/MainActivity.java
+++ android/Gezinti/app/src/main/java/com/hackathon/gezinti/activity/MainActivity.java
@@ -1,43 +1,18 @@
 package com.hackathon.gezinti.activity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.BottomSheetBehavior;
 import android.util.Log;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.Spinner;
 
-import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
-import com.google.android.gms.common.GooglePlayServicesRepairableException;
-import com.google.android.gms.common.api.Status;
-import com.google.android.gms.location.places.Place;
-import com.google.android.gms.location.places.ui.PlaceAutocomplete;
 import com.google.android.gms.maps.model.LatLng;
 import com.hackathon.gezinti.R;
-import com.hackathon.gezinti.fragment.BottomSheetListFragment;
 import com.hackathon.gezinti.fragment.MapFragment;
-import com.hackathon.gezinti.interfaces.EventSearchListener;
-import com.hackathon.gezinti.models.common.Event;
-import com.hackathon.gezinti.models.request.EventSearchRequest;
-import com.hackathon.gezinti.models.response.EventSearchResponse;
-import com.hackathon.gezinti.network.EventInteractor;
 
-import java.util.ArrayList;
-
-public class MainActivity extends BaseActivity implements View.OnClickListener, BottomSheetListFragment.BottomSheetInteraction {
-    private BottomSheetBehavior<View> mBottomSheetBehavior;
-    private BottomSheetListFragment mBottomSheetListFragment;
-
+public class MainActivity extends BaseActivity implements View.OnClickListener {
     private MapFragment mMapFragment;
     private Button mButtonRefresh;
-    private Spinner mEventsSpinner, mTimesSpinner;
-    private EventInteractor mEventInteractor;
-    private static int PLACE_AUTOCOMPLETE_REQUEST_CODE = 1;
-    public static int EVENT_DETAIL_REQUEST_CODE = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,32 +30,8 @@ public class MainActivity extends BaseActivity implements View.OnClickListener, 
     }
 
     private void initViews() {
-        mBottomSheetListFragment = (BottomSheetListFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentBottomSheet);
-        mBottomSheetListFragment.setDimBackground(findViewById(R.id.dim_background));
-        mBottomSheetBehavior = BottomSheetBehavior.from(findViewById(R.id.fragmentBottomSheet));
-        mBottomSheetBehavior.setHideable(false);
-        mBottomSheetBehavior.setBottomSheetCallback(mBottomSheetListFragment.getBottomSheetCallback());
-
-        ArrayList<Event> events = new ArrayList<>();
-        mBottomSheetListFragment.setEventsForPosition(events);
-
         mButtonRefresh = (Button) findViewById(R.id.btn_refresh);
         mButtonRefresh.setOnClickListener(this);
-
-        mEventsSpinner = (Spinner) findViewById(R.id.sp_events);
-        ArrayAdapter<CharSequence> eventAdapter = ArrayAdapter
-                .createFromResource(this, R.array.array_event_types,
-                        android.R.layout.simple_spinner_item);
-        eventAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        mEventsSpinner.setAdapter(eventAdapter);
-
-        mTimesSpinner = (Spinner) findViewById(R.id.sp_times);
-        ArrayAdapter<CharSequence> timeAdapter = ArrayAdapter
-                .createFromResource(this, R.array.array_event_times,
-                        android.R.layout.simple_spinner_item);
-        timeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        mTimesSpinner.setAdapter(timeAdapter);
-
     }
 
     @Override
@@ -98,10 +49,11 @@ public class MainActivity extends BaseActivity implements View.OnClickListener, 
         double longitude = latLng.longitude;
         double latitude = latLng.latitude;
 
-        int spEventsPosition = mEventsSpinner.getSelectedItemPosition();
-        int spTimesPosition = mTimesSpinner.getSelectedItemPosition();
+        //int spEventsPosition = mEventsSpinner.getSelectedItemPosition();
+        //int spTimesPosition = mTimesSpinner.getSelectedItemPosition();
 
         Log.e("MainAct", "Lat: " + latitude + " Lon: " + longitude);
+        /*
         mEventInteractor = new EventInteractor(this);
         EventSearchRequest eventSearchRequest = new EventSearchRequest();
         eventSearchRequest.setLat(String.valueOf(latitude));
@@ -130,6 +82,7 @@ public class MainActivity extends BaseActivity implements View.OnClickListener, 
                 dismissWaitingDialog();
             }
         });
+        */
     }
 
     @Override
@@ -138,51 +91,4 @@ public class MainActivity extends BaseActivity implements View.OnClickListener, 
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.action_search) {
-            try {
-                Intent intent =
-                        new PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_OVERLAY)
-                                .build(this);
-                startActivityForResult(intent, PLACE_AUTOCOMPLETE_REQUEST_CODE);
-            } catch (GooglePlayServicesRepairableException e) {
-                // TODO: Handle the error.
-            } catch (GooglePlayServicesNotAvailableException e) {
-                // TODO: Handle the error.
-            }
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == PLACE_AUTOCOMPLETE_REQUEST_CODE) {
-            if (resultCode == RESULT_OK) {
-                Place place = PlaceAutocomplete.getPlace(this, data);
-                Log.d("MainAct", place.getLatLng().latitude + " " + place.getLatLng().longitude);
-                mMapFragment.moveMapsCamera(place.getLatLng());
-
-                // TODO: MAKE NETWORK REQUEST - USE getLatLng
-
-            } else if (resultCode == PlaceAutocomplete.RESULT_ERROR) {
-                Status status = PlaceAutocomplete.getStatus(this, data);
-                // TODO: Handle the error.
-                Log.i("MainAct", status.getStatusMessage());
-
-            } else if (resultCode == RESULT_CANCELED) {
-                // The user canceled the operation.
-            }
-        }
-        if (requestCode == EVENT_DETAIL_REQUEST_CODE) {
-            if (resultCode == RESULT_OK) {
-                findNearByEvents();
-            }
-        }
-    }
-
-    @Override
-    public void onEventCreated() {
-        findNearByEvents();
-    }
 }
--- android/Gezinti/app/src/main/java/com/hackathon/gezinti/fragment/MapFragment.java
+++ android/Gezinti/app/src/main/java/com/hackathon/gezinti/fragment/MapFragment.java
@@ -27,12 +27,8 @@ import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.maps.model.Polygon;
-import com.google.android.gms.maps.model.PolygonOptions;
-import com.hackathon.gezinti.activity.EventDetailActivity;
-import com.hackathon.gezinti.activity.MainActivity;
 import com.hackathon.gezinti.R;
+import com.hackathon.gezinti.activity.EventDetailActivity;
 import com.hackathon.gezinti.models.common.Coordinates;
 import com.hackathon.gezinti.models.common.Event;
 
@@ -41,7 +37,7 @@ import java.util.List;
 
 public class MapFragment extends Fragment implements OnMapReadyCallback,
         GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,
-        LocationListener, GoogleMap.OnInfoWindowClickListener, GoogleMap.OnMapLongClickListener {
+        LocationListener, GoogleMap.OnInfoWindowClickListener {
 
     public static MapFragment instance;
 
@@ -49,8 +45,6 @@ public class MapFragment extends Fragment implements OnMapReadyCallback,
     private MapView mapView;
     private GoogleMap googleMap;
     private GoogleApiClient mGoogleApiClient;
-    private Location mLastLocation;
-    private Marker mCurrLocationMarker;
     private LocationRequest mLocationRequest;
 
     private ArrayList<Event> mEvents;
@@ -113,7 +107,6 @@ public class MapFragment extends Fragment implements OnMapReadyCallback,
         moveMapsCamera(latLng);
 
         googleMap.setOnInfoWindowClickListener(this);
-        googleMap.setOnMapLongClickListener(this);
     }
 
     protected synchronized void buildGoogleApiClient() {
@@ -141,10 +134,7 @@ public class MapFragment extends Fragment implements OnMapReadyCallback,
 
     @Override
     public void onLocationChanged(Location location) {
-        mLastLocation = location;
-        if (mCurrLocationMarker != null) {
-            mCurrLocationMarker.remove();
-        }
+        //mLastLocation = location;
 
         //Focus current location
         LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
@@ -164,37 +154,18 @@ public class MapFragment extends Fragment implements OnMapReadyCallback,
         googleMap.clear();
         Marker marker;
 
+        // mark all events for coordinates
+        /*
         int count = 0;
         for (Event item : events) {
-            if (item.getCoordinatesList().size() > 1) {
-                Polygon polygon = googleMap.addPolygon(new PolygonOptions()
-                    .addAll(coordinateToLatLng(item.getCoordinatesList()))
-                    .strokeColor(0xFF00AA00)
-                    .fillColor(0x2200FFFF)
-                    .strokeWidth(2)
-                );
-                polygon.setTag(count);
-                count++;
-                polygon.setClickable(true);
-                googleMap.setOnPolygonClickListener(new GoogleMap.OnPolygonClickListener() {
-                    @Override
-                    public void onPolygonClick(Polygon polygon) {
-                        Intent intent = new Intent(getActivity(), EventDetailActivity.class);
-                        Bundle bundle = new Bundle();
-                        bundle.putSerializable("EventDetail", mEvents.get((int) polygon.getTag()));
-                        intent.putExtras(bundle);
-                        getActivity().startActivityForResult(intent, MainActivity.EVENT_DETAIL_REQUEST_CODE);
-                    }
-                });
-            } else {
-                marker = this.googleMap.addMarker(new MarkerOptions()
-                        .position(new LatLng(Double.valueOf(item.getCoordinatesList().get(0).getLat()), Double.valueOf(item.getCoordinatesList().get(0).getLon())))
-                        .title(item.getTitle())
-                );
-                marker.setTag(count);
-                count++;
-            }
+            marker = this.googleMap.addMarker(new MarkerOptions()
+                    .position(new LatLng(Double.valueOf(item.getCoordinatesList().get(0).getLat()), Double.valueOf(item.getCoordinatesList().get(0).getLon())))
+                    .title(item.getTitle())
+            );
+            marker.setTag(count);
+            count++;
         }
+        */
     }
 
     public LatLng getCenterOfScreen() {
@@ -206,6 +177,7 @@ public class MapFragment extends Fragment implements OnMapReadyCallback,
         googleMap.animateCamera(CameraUpdateFactory.zoomTo(13));
     }
 
+    // markers info text
     @Override
     public void onInfoWindowClick(Marker marker) {
         Log.d("MapFragment", marker.getTag().toString());
@@ -216,7 +188,7 @@ public class MapFragment extends Fragment implements OnMapReadyCallback,
         Bundle bundle = new Bundle();
         bundle.putSerializable("EventDetail", mEvents.get((int) marker.getTag()));
         intent.putExtras(bundle);
-        getActivity().startActivityForResult(intent, MainActivity.EVENT_DETAIL_REQUEST_CODE);
+        //getActivity().startActivityForResult(intent, MainActivity.EVENT_DETAIL_REQUEST_CODE);
     }
 
     public List<LatLng> coordinateToLatLng(List<Coordinates> coordinatesList){
@@ -229,83 +201,6 @@ public class MapFragment extends Fragment implements OnMapReadyCallback,
         return latLngList;
     }
 
-
-    //
-    //  Event Create Methods
-    //
-    List<Marker> mMarkerList;
-    ArrayList<Coordinates> mCoordinatesList;
-    boolean mCreatorMode = false;
-    public void openEventCreatorMode(){
-        googleMap.clear();
-        if(mCoordinatesList == null) {
-            mCoordinatesList = new ArrayList<>();
-            mMarkerList = new ArrayList<>();
-        }
-        mCoordinatesList.clear();
-        mMarkerList.clear();
-        mCreatorMode = true;
-    }
-    public void closeEventCreatorMode(){
-        googleMap.clear();
-        mCoordinatesList.clear();
-        mMarkerList.clear();
-        mCreatorMode = false;
-    }
-    private Marker tempMarker;
-    @Override
-    public void onMapLongClick(LatLng latLng) {
-        if(mCreatorMode){
-            tempMarker = this.googleMap.addMarker(new MarkerOptions()
-                    .position(latLng)
-                    .title(""+mCoordinatesList.size())
-            );
-            mMarkerList.add(tempMarker);
-            mCoordinatesList.add(new Coordinates(String.valueOf(latLng.latitude), String.valueOf(latLng.longitude)));
-        }
-    }
-    public void popCoordinate(){
-        if(mCoordinatesList != null && mCoordinatesList.size() > 0){
-            mCoordinatesList.remove(mCoordinatesList.size()-1);
-            Marker temp = mMarkerList.get(mMarkerList.size()-1);
-            mMarkerList.remove(mMarkerList.size()-1);
-            temp.remove();
-        }
-    }
-    public ArrayList<Coordinates> getCoordinatesList(){
-        if(mCoordinatesList != null && mCoordinatesList.size()>0)
-            return mCoordinatesList;
-        return null;
-    }
-    //
-    //  Event Create Methods
-    //
-
-
-
-    //deprecated
-    public void makeRequestAndPin() {
-
-        if (getLocation) {
-            this.googleMap.clear();
-            LatLng tempCoordinat = new LatLng(41.08, 29.02);
-            this.googleMap.addMarker(new MarkerOptions().position(tempCoordinat)
-                    .title("Test - 1"));
-            LatLng tempCoordinat2 = new LatLng(40.08, 29.02);
-            this.googleMap.addMarker(new MarkerOptions().position(tempCoordinat2)
-                    .title("Test - 2"));
-        } else {
-            this.googleMap.clear();
-            LatLng tempCoordinat = new LatLng(41.08, 29.22);
-            this.googleMap.addMarker(new MarkerOptions().position(tempCoordinat)
-                    .title("Test - 3"));
-            LatLng tempCoordinat2 = new LatLng(40.08, 29.32);
-            this.googleMap.addMarker(new MarkerOptions().position(tempCoordinat2)
-                    .title("Test - 4"));
-        }
-        getLocation = !getLocation;
-
-    }
     public boolean checkLocationPermission() {
         if (ContextCompat.checkSelfPermission(getActivity(),
                 FINE_LOCATION[0])
